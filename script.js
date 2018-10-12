4fb350e058ce7e15153f5435425016f022b61ce0
theodorabirch <theodorabirch@gmail.com>
2018-10-12 16:18:48 +0100

    the layout of the border ready for the flip function

---
+++ script.js
@@ -0,0 +1,17 @@
+// create a function that builds the grid
+//
+// function createGrid(i) {
+//   for(let rows = 0; rows < i; rows++) {
+//     for(let columns = 0; columns < i; columns++); {
+//       $('.game-container').append('<div class=grid></div>');
+//     }
+//   }
+//   $('.grid').width(960/i);
+//   $('.grid').height(960/i);
+// }
+
+
+const memoryArray = [ 'A', 'A', 'B', 'B' , 'C', 'C', 'D', 'D'];
+const memoryValues = [];
+const memorySquareIds = [];
+const squaresFlipped = 0; 
