cd854dea51857ed72e5c18c7a2bd489d3da4d944
theodorabirch <theodorabirch@gmail.com>
2018-10-15 15:27:15 +0100

    Added in two player logic to javascript and fixed the event listener

--- script.js
+++ script.js
@@ -49,10 +49,17 @@ const cardsArray = [{
   'img': 'images/prawn.png'
 }
 ];
+
+
+//Shuffle of the board
+
 // need to duplicate the array so I can create a match for each card
 const gameGrid = cardsArray.concat(cardsArray);
 //need to randomise the array
 const shuffledArray = gameGrid; //.sort(() => 0.5 - Math.random());
+
+const playerOne = { id: 1, name: 'Player 1', score: 0 };
+const playerTwo = { id: 2, name: 'Player 2', score: 0 };
 
 // define the global variables that are used.
 let firstChoice = '';
@@ -60,8 +67,11 @@ let secondChoice = '';
 let count = 0;
 let previousChoice = null;
 const delay = 1200;
+let currentPlayer = playerOne;
+
 // Now I need to grab the id of the div in html
 
+// Create the grid
 const game = document.getElementById('game');
 // will create a section with a class of grid
 const grid = document.createElement('section');
@@ -88,6 +98,7 @@ shuffledArray.forEach(item => {
   back.classList.add('back');
   // apply the background image of the div to the cardsArray image
   front.style.backgroundImage = `url(${item.img})`;
+  // console.log('this is the image', item.img);
   // card.style.backgroundImage = `url`
   // append the div to the grid section
   // console.log(back);
@@ -99,8 +110,9 @@ shuffledArray.forEach(item => {
 const match = () => {
   const selected = document.querySelectorAll('.selected');
   selected.forEach(card => {
-    console.log('Adding match to', card);
+    // console.log('Adding match to', card);
     card.classList.add('match');
+    // event.target.removeEventListener('click', lookForAMatch);
   });
 };
 
@@ -111,39 +123,54 @@ const resetGuesses = () => {
 
   const selected = document.querySelectorAll('.selected');
   selected.forEach(card => {
-    card.classList.remove('selected')
+    card.classList.remove('selected');
   });
 };
-
 // add an eventlistener to the entire grid
-grid.addEventListener('click', function(event) {
+const domCard = document.querySelectorAll('.back');
+
+// grid.addEventListener('click', lookForAMatch);
+
+domCard.forEach(card => card.addEventListener('click', lookForAMatch));
+let firstDomElement;
+
+function lookForAMatch(event) {
   const clicked = event.target;
   clicked.style.zIndex = -1;
-  console.log('is this working', clicked); // this is working.
+  // console.log('is this working', clicked); // this is working.
   if (clicked.nodeName === 'section' || clicked === previousChoice || clicked.parentNode.classList.contains('selected')) {
     return;
   }
   if (count < 2) {
     count++;
     if (count === 1) {
+      firstDomElement = event.target;
       firstChoice = clicked.parentNode.dataset.name;
-      console.log('is first choice working', firstChoice);
+      // console.log('is first choice working', firstChoice);
       clicked.parentNode.classList.add('selected');
     } else {
       secondChoice = clicked.parentNode.dataset.name;
-      console.log('second choice' , secondChoice);
+      // console.log('second choice is working' , secondChoice);
       clicked.parentNode.classList.add('selected');
     }
     if (firstChoice !== '' && secondChoice !== '') {
       if(firstChoice === secondChoice) {
-        console.log('MATCH!');
+        currentPlayer.score++;
+        // console.log('===>', event.target);
+        firstDomElement.removeEventListener('click', lookForAMatch);
+        // console.log('--->', firstDomElement);
+        // console.log('MATCH!');
         setTimeout(match, delay);
         // setTimeout(resetGuesses, delay);
       } else {
         setTimeout(resetGuesses, delay);
+        if (currentPlayer.id === 1) {
+          currentPlayer = playerTwo;
+          // console.log(currentPlayer);
+        }
       }
     }
     previousChoice = clicked;
 
   }
-});
+}
